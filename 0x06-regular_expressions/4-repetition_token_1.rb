f4934f2b51c35136074b76723323d25eb08c0712
Yemmy22 <opeyemiadedigba20@gmail.com>
2024-02-27 09:28:52 +0000

    Matches a string with single characters and 0 or more occurence of a character

---
+++ 0x06-regular_expressions/4-repetition_token_1.rb
@@ -0,0 +1,4 @@
+#!/usr/bin/env ruby
+# Regular Expression matches any string starting with single occurrence of 'h' and 'b' followed by 0 or more occurrences of 't' and ends with a single occurrence of 'n'.
+puts ARGV[0].scan(/^hb(t*)n$/).join
+
