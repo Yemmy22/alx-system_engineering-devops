5018567d848fb426ef8c93a331a8afe7987447e6
Yemmy22 <opeyemiadedigba20@gmail.com>
2024-02-27 11:57:38 +0000

    Fix error in code base

--- 0x06-regular_expressions/5-beginning_and_end.rb
+++ 0x06-regular_expressions/5-beginning_and_end.rb
@@ -1,4 +1,4 @@
 #!/usr/bin/env ruby
 # Regular expression matches any string beginning with a single 'h', followed by any single character and ends with a single 'n'
-puts ARGV[0].scan(/^h(.)n$/).join
+puts ARGV[0].scan(/^h.n$/).join
 
