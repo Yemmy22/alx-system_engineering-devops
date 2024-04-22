484d7b0a9819b04161b232ae95636bbc2500f685
Yemmy22 <opeyemiadedigba20@gmail.com>
2024-04-22 10:33:58 +0000

    Create a file in a specified directory

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# This script creates a file with a string in the '/tmp' folder with specified file permission
+
+file { '/tmp/school':
+  ensure  => file,
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}
