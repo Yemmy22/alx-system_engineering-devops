1760b188cab54dd15b99689393943fcf36a0001f
Yemmy22 <opeyemiadedigba20@gmail.com>
2024-04-23 08:05:38 +0000

    Fix to pass puppet-lint

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -9,7 +9,7 @@ package {'flask':
   require  => Package['python3-pip'],
 }
 package { 'werkzeug':
-  ensure  => '2.1.1',
+  ensure   => '2.1.1',
   provider => 'pip3',
   require  => Package['python3-pip'],
 }
