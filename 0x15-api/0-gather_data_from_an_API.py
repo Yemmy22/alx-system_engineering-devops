9540da102ff3b358ae55d40b24728cc12c809427
Yemmy22 <opeyemiadedigba20@gmail.com>
2024-07-30 01:16:30 +0000

    Returns information about an employee's TODO list progress

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+from sys import argv
+import requests
+
+
+if len(argv) == 2:
+
+    url = f"https://jsonplaceholder.typicode.com/users/{argv[1]}"
+    todos_url = f'https://jsonplaceholder.typicode.com/users/{argv[1]}/todos'
+    user_response = requests.get(url)
+    EMPLOYEE_NAME = user_response.json().get('name')
+
+    todos = requests.get(todos_url)
+
+    NUMBER_OF_DONE_TASKS = 0
+    TOTAL_NUMBER_OF_TASKS = 0
+    TASK_TITLE = []
+
+    for todo in todos.json():
+        TOTAL_NUMBER_OF_TASKS += 1
+        if todo.get('completed'):
+            NUMBER_OF_DONE_TASKS += 1
+            TASK_TITLE.append(todo)
+
+
+if __name__ == "__main__":
+    print(f"Employee EMPLOYEE_NAME is done with tasks({NUMBER_OF_DONE_TASKS}/{TOTAL_NUMBER_OF_TASKS}):")
+
+    for task in TASK_TITLE:
+        print("\t {}".format(task.get('title')))
